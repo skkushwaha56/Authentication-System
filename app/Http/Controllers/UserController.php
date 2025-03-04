4efeba4e848b844f14f5fb0f3c003067038bcba2
skkushwaha56 <skkushwaha56@gmail.com>
2025-03-04 15:59:10 +0530

    new change

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     //
     public function register(){
         return view('registration');
+    }
+
+    public function store(Request $request){
+
+        $validate = $request->validate([
+            'name' =>  'required | string',
+            'email' => 'required | string',
+            'password' => 'required|string'
+            
+        ]);
+
+        User::created([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+    }
+
+    public function get($id){
+        $data = User::find($id);
+    }
+
+    public function update(request $request, $id){
+        $request->validate([
+            'name' => 'required|string|unique:users,name,' . $id,
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = user::findOrFail($id);
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+    }
+
+    public function destroy($id){
+        $user = User::find($id);
+
+        $user->delete();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,10 @@ Route::get('/', function () {
 
 
 Route::get('registration',[UserController::class,'register']);
+Route::post('registration/store',[UserController::class,'store']);
+Route::get('registration/edit',[UserController::class,'edit']);
+Route::put('registration/update',[UserController::class,'update']);
+Route::delete('registration/delete',[UserController::class,'destroy']);
+
 Route::view('login','login');
 Route::view('dashboard','dashboard');
