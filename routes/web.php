a89f98ef185c7a41901b237842127bcbb3f2d5aa
skkushwaha56 <skkushwaha56@gmail.com>
2025-03-04 13:19:25 +0530

    change new

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -7,6 +8,7 @@ Route::get('/', function () {
 });
 
 
+
+Route::get('registration',[UserController::class,'register']);
 Route::view('login','login');
-Route::view('registration','registration');
 Route::view('dashboard','dashboard');
